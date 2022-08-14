1fe26d4fc7443e795d5ead3d781e036915672c3d
Anthony Walker <walker.anthony91@gmail.com>
2022-08-14 18:09:04 +0000

    added progress bar for downloading data, and updated readme.

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # PyAURN - [![latest-version](https://img.shields.io/pypi/v/pyaurn)](https://pypi.org/project/pyaurn)
 
-https://img.shields.io/pypi/l/pyaurn
 This is a reworked fork of the `robintw/PyAURN` utilising pyreadr instead of rpy2. This negates having a local installation of R, and therefore can remain a python only configuration. 
 
 This is a simple Python package to import data from the UK Automatic Urban Rural Network - an air pollution network run by the UK government.
@@ -23,8 +22,13 @@ from pyaurn import importAURN, importMetadata
 # Download metadata of site IDs, names, locations etc
 metadata = importMetadata()
 
-# Download 4 years of data for the Southampton Central site
-# (SOUT is the site ID for this site)
-# Note: range(2015, 2019) will produce a list of four years: 2015, 2016, 2017 and 2018
-data = importAURN("SOUT", range(2015, 2019))
+# Download 4 years of data for the Marylebone Road site
+# (MY1 is the site ID for this site)
+# Note: range(2016, 2022) will produce a list of six years: 2016, 2017, 2018, 2019, 2020, and 2021. Alternatively define a list of years to use eg. [2016,2017,2018,2019,2020,2021]
+data = importAURN("MY1", range(2016, 2022))
 ```
+
+## Future developments
+
+* integrate other openair functions
+* open to suggestions 
--- src/pyaurn.py
+++ src/pyaurn.py
@@ -3,10 +3,36 @@ from urllib.error import HTTPError
 import warnings
 import pandas as pd
 import pyreadr
+import progress
+from tqdm import tqdm
+
+def my_hook(t):
+    """Wraps tqdm instance
+    """
+    last_b = [0]
+
+    def update_to(b=1, bsize=1, tsize=None):
+        """
+        b  : int, optional
+            Number of blocks transferred so far [default: 1].
+        bsize  : int, optional
+            Size of each block (in tqdm units) [default: 1].
+        tsize  : int, optional
+            Total size (in tqdm units). If [default: None] remains unchanged.
+        """
+        if tsize is not None:
+            t.total = tsize
+        t.update((b - last_b[0]) * bsize)
+        last_b[0] = b
+
+    return update_to
 
 
-def _download_and_import_RData_file(url):
-    filename, headers = urlretrieve(url)
+
+def _download_and_import_RData_file(url,year,site):
+    with tqdm(unit = 'B', unit_scale = True, unit_divisor = 1024, miniters = 1, desc = f"Downloading {site} {year} data:") as t:
+        filename, headers = urlretrieve(url, reporthook = my_hook(t))
+    
 
     # Load the RData file into R and get the name of the new variable created
     r_obj_name = pyreadr.read_r(filename)
@@ -34,7 +60,7 @@ def importAURN(site, years):
         url = f"https://uk-air.defra.gov.uk/openair/R_data/{site}_{year}.RData"
 
         try:
-            df = _download_and_import_RData_file(url)
+            df = _download_and_import_RData_file(url,year,site)
         except HTTPError:
             errors_raised = True
             continue
@@ -61,4 +87,4 @@ def importMetadata():
 
     df = df.drop_duplicates(subset=['site_id'])
     
-    return df
+    return 
